99c1bf1df27fb608e86d926cc3a6fad751de907f
hyemdev <devdev.hyem@gmail.com>
2023-06-09 18:03:33 +0900

    feat: news 섹션 기능(swiper, json) 완료 / info -ing

--- src/pages/SongList.js
+++ src/pages/SongList.js
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 const SongList = ({ songs }) => {
     console.log(songs);
     const list = songs.map((item) => {
-        // retur()는 jsx html을 만들려고 한다는 의도
+        // return()는 jsx html을 만들려고 한다는 의도
         return (
             <li className="list-group-item" key={item.id}>
                 <Link to={`/songlist/${item.id}`} style={{ textDecoration: "none" }}>
